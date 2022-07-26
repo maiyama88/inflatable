02754cbd829b6e7a3f54b12cc76cb3417ebdc517
maiyama88 <7319136@ed.tus.ac.jp>
2022-07-26 14:36:47 +0900

    変更
    変更

--- ADCDACPI.py
+++ ADCDACPI.py
@@ -77,7 +77,7 @@ class ADCDACPi(object):
         if (mode > 1) or (mode < 0):
             raise ValueError('read_adc_voltage: mode out of range')
         raw = self.read_adc_raw(channel, mode)
-        self.read_adc_raw = (3.3 * (raw - 9) / (3323 - 9))
+        raw = (3.3 * (self.__adcrefvoltage - 9) / (3323 - 9))
         voltage = float((self.__adcrefvoltage / 4096) * raw)
         return voltage
 
