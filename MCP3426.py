f4614920c8ba2a685fa22eca94ed8e4670944a93
maiyama88 <7319136@ed.tus.ac.jp>
2022-11-16 16:34:04 +0900

    MCP3426

--- MCP3426.py
+++ MCP3426.py
@@ -1,50 +1,71 @@
+# Distributed with a free-will license.
+# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+# MCP3426
+# This code is designed to work with the MCP3426_I2CADC I2C Mini Module available from ControlEverything.com.
+# https://www.controleverything.com/content/Analog-Digital-Converters?sku=MCP3426_I2CADC#tabs-0-product_tabset-2
+
+import smbus
 import time
-import Adafruit_ADS1x15
-# ADS1015の設定をします
-ads = Adafruit_ADS1x15.ADS1015(address=0x68, busnum=1)
 
-# ゲインの設定です
-# 指定によって、取得できる電圧の範囲が変わります
+# Get I2C bus
+bus = smbus.SMBus(1)
 
-#  - 2/3 = +/-6.144V
-#  -   1 = +/-4.096V
-#  -   2 = +/-2.048V
-#  -   4 = +/-1.024V
-#  -   8 = +/-0.512V
-#  -  16 = +/-0.256V
-GAIN = 1
+# I2C address of the device
+MCP3426_DEFAULT_ADDRESS				= 0x68
 
-#
-#「ゲイン1」の時の、「受信データ1単位」の電圧を計算します
-#
+# MCP3426 Configuration Command Set
+MCP3426_CMD_NEW_CNVRSN				= 0x80 # Initiate a new conversion(One-Shot Conversion mode only)
+MCP3426_CMD_CHNL_1					= 0x00 # Channel-1 Selected
+MCP3426_CMD_CHNL_2					= 0x20 # Channel-2 Selected
+MCP3426_CMD_MODE_CONT				= 0x10 # Continuous Conversion Mode
+MCP3426_CMD_MODE_ONESHOT			= 0x00 # One-Shot Conversion Mode
+MCP3426_CMD_SPS_240					= 0x00 # 240 SPS (12-bit)
+MCP3426_CMD_SPS_60					= 0x04 # 60 SPS (14-bit)
+MCP3426_CMD_SPS_15					= 0x08 # 15 SPS (16-bit)
+MCP3426_CMD_GAIN_1					= 0x00 # PGA Gain = 1V/V
+MCP3426_CMD_GAIN_2					= 0x01 # PGA Gain = 2V/V
+MCP3426_CMD_GAIN_4					= 0x02 # PGA Gain = 4V/V
+MCP3426_CMD_GAIN_8					= 0x03 # PGA Gain = 8V/V
+MCP3426_CMD_READ_CNVRSN				= 0x00 # Read Conversion Result Data
 
-# 「ゲイン1」の範囲はプラス・マイナス 4.096なので、8.192になります
-RANGE = 4.096 * 2
+class MCP3426():
+	def set_channel(self):
+		"""Select the Channel user want to use from 1-2"""
+		self.channel = int(input("Enter the Channel No. = "))
+		while self.channel < 1 or self.channel > 2:
+			self.channel = int(input("Enter the Channel No. = "))
+		
+		return self.channel
+	
+	def config_command(self):
+		"""Select the Configuration Command from the given provided values"""
+		if self.channel == 1:
+			CONFIG_CMD = (MCP3426_CMD_MODE_CONT | MCP3426_CMD_SPS_240 | MCP3426_CMD_GAIN_1 | MCP3426_CMD_CHNL_1)
+		elif self.channel == 2:
+			CONFIG_CMD = (MCP3426_CMD_MODE_CONT | MCP3426_CMD_SPS_240 | MCP3426_CMD_GAIN_1 | MCP3426_CMD_CHNL_2)
+		
+		bus.write_byte(MCP3426_DEFAULT_ADDRESS, CONFIG_CMD)
+	
+	def read_adc(self):
+		"""Read data back from MCP3426_CMD_READ_CNVRSN(0x00), 2 bytes
+		raw_adc MSB, raw_adc LSB"""
+		data = bus.read_i2c_block_data(MCP3426_DEFAULT_ADDRESS, MCP3426_CMD_READ_CNVRSN, 2)
+		
+		# Convert the data to 12-bits
+		raw_adc = ((data[0] & 0x0F) * 256) + data[1]
+		if raw_adc > 2047 :
+			raw_adc -= 4095
+		
+		return {'r' : raw_adc}
 
-# ADSは上記範囲を12bit(4096)で表すので、上記範囲を4096で割ると
-# ADSのデータ「1」につき、電圧は0.002[V]になります。
-# UNIT=単位
-UNIT = RANGE / 4096
+from MCP3426 import MCP3426
+mcp3426 = MCP3426()
 
-# ループを100回繰り返します
-for i in range(100):
-
-    # ADSのアナログ入力ピンを指定します
-    ads1015_pin  = 0
-
-    data = 0
-    volt = 0
-
-    # ADS1015からデータを取得します
-    data = ads.read_adc( ads1015_pin, gain=GAIN)
-
-    # 取得データから電圧を計算します
-    volt = data * UNIT
-
-    print("受信データ：" + str(data))
-    print("電圧      ：" + "{:.3f}".format(volt))
-
-    # 1秒待機します
-    time.sleep(1)
-
-print("done.")
+while True :
+	mcp3426.set_channel()
+	mcp3426.config_command()
+	time.sleep(0.1)
+	adc = mcp3426.read_adc()
+	print ("Digital Value of Analog Input : %d "%(adc['r']))
+	print (" ********************************* ")
+	time.sleep(0.8)
