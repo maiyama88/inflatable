f56b613adee4ba4042d131407bd8487ae3f1f4b3
maiyama88 <7319136@ed.tus.ac.jp>
2022-11-16 16:48:23 +0900

    変更

--- ADCDACPI.py
+++ ADCDACPI.py
@@ -8,205 +8,79 @@ Based on the Microchip MCP3202 and MCP4822 (new MCP3426)
 
 from __future__ import absolute_import, division, print_function, \
                                                     unicode_literals
-
 try:
-    import i2cdev
+    import MCP3426
 except ImportError:
     raise ImportError(
-        "i2cdev not found.")
+        "MCP3426 not found.")
 
 class ADCDACPi(object):
     """
     Based on the Microchip MCP3202 and MCP4822 (new MCP3426)
     """
+import smbus
 import time
-import Adafruit_ADS1x15
-# ADS1015の設定をします
-ads = Adafruit_ADS1x15.ADS1015(address=0x68, busnum=1)
 
-# ゲインの設定です
-# 指定によって、取得できる電圧の範囲が変わります
+# Get I2C bus
+bus = smbus.SMBus(1)
 
-#  - 2/3 = +/-6.144V
-#  -   1 = +/-4.096V
-#  -   2 = +/-2.048V
-#  -   4 = +/-1.024V
-#  -   8 = +/-0.512V
-#  -  16 = +/-0.256V
-GAIN = 1
+# I2C address of the device
+MCP3426_DEFAULT_ADDRESS				= 0x68
 
-#
-#「ゲイン1」の時の、「受信データ1単位」の電圧を計算します
-#
+# MCP3426 Configuration Command Set
+MCP3426_CMD_NEW_CNVRSN				= 0x80 # Initiate a new conversion(One-Shot Conversion mode only)
+MCP3426_CMD_CHNL_1					= 0x00 # Channel-1 Selected
+MCP3426_CMD_CHNL_2					= 0x20 # Channel-2 Selected
+MCP3426_CMD_MODE_CONT				= 0x10 # Continuous Conversion Mode
+MCP3426_CMD_MODE_ONESHOT			= 0x00 # One-Shot Conversion Mode
+MCP3426_CMD_SPS_240					= 0x00 # 240 SPS (12-bit)
+MCP3426_CMD_SPS_60					= 0x04 # 60 SPS (14-bit)
+MCP3426_CMD_SPS_15					= 0x08 # 15 SPS (16-bit)
+MCP3426_CMD_GAIN_1					= 0x00 # PGA Gain = 1V/V
+MCP3426_CMD_GAIN_2					= 0x01 # PGA Gain = 2V/V
+MCP3426_CMD_GAIN_4					= 0x02 # PGA Gain = 4V/V
+MCP3426_CMD_GAIN_8					= 0x03 # PGA Gain = 8V/V
+MCP3426_CMD_READ_CNVRSN				= 0x00 # Read Conversion Result Data
 
-# 「ゲイン1」の範囲はプラス・マイナス 4.096なので、8.192になります
-RANGE = 4.096 * 2
+class MCP3426():
+	def set_channel(self):
+		"""Select the Channel user want to use from 1-2"""
+		self.channel = int(input("Enter the Channel No. = "))
+		while self.channel < 1 or self.channel > 2:
+			self.channel = int(input("Enter the Channel No. = "))
+		
+		return self.channel
+	
+	def config_command(self):
+		"""Select the Configuration Command from the given provided values"""
+		if self.channel == 1:
+			CONFIG_CMD = (MCP3426_CMD_MODE_CONT | MCP3426_CMD_SPS_240 | MCP3426_CMD_GAIN_1 | MCP3426_CMD_CHNL_1)
+		elif self.channel == 2:
+			CONFIG_CMD = (MCP3426_CMD_MODE_CONT | MCP3426_CMD_SPS_240 | MCP3426_CMD_GAIN_1 | MCP3426_CMD_CHNL_2)
+		
+		bus.write_byte(MCP3426_DEFAULT_ADDRESS, CONFIG_CMD)
+	
+	def read_adc(self):
+		"""Read data back from MCP3426_CMD_READ_CNVRSN(0x00), 2 bytes
+		raw_adc MSB, raw_adc LSB"""
+		data = bus.read_i2c_block_data(MCP3426_DEFAULT_ADDRESS, MCP3426_CMD_READ_CNVRSN, 2)
+		
+		# Convert the data to 12-bits
+		raw_adc = ((data[0] & 0x0F) * 256) + data[1]
+		if raw_adc > 2047 :
+			raw_adc -= 4095
+		
+		return {'r' : raw_adc}
 
-# ADSは上記範囲を12bit(4096)で表すので、上記範囲を4096で割ると
-# ADSのデータ「1」につき、電圧は0.002[V]になります。
-# UNIT=単位
-UNIT = RANGE / 4096
+from MCP3426 import MCP3426
+mcp3426 = MCP3426()
 
-# ループを100回繰り返します
-for i in range(100):
+while True :
+	mcp3426.set_channel()
+	mcp3426.config_command()
+	time.sleep(0.1)
+	adc = mcp3426.read_adc()
+	print ("Digital Value of Analog Input : %d "%(adc['r']))
+	print (" ********************************* ")
+	time.sleep(0.8)
 
-    # ADSのアナログ入力ピンを指定します
-    ads1015_pin  = 0
-
-    data = 0
-    volt = 0
-
-    # ADS1015からデータを取得します
-    data = ads.read_adc( ads1015_pin, gain=GAIN)
-
-    # 取得データから電圧を計算します
-    volt = data * UNIT
-
-    print("受信データ：" + str(data))
-    print("電圧      ：" + "{:.3f}".format(volt))
-
-    # 1秒待機します
-    time.sleep(1)
-
-   
-
-    __dacMaxOutput__ = {
-        1: 2.048,  # This is Vref
-        2: 3.3  # This is the voltage of the 3.3V rail
-    }
-
-    # public methods
-    def __init__(self, gain_factor=1):
-        """
-        Class Constructor - Initialise the DAC
-        :param gain_factor: Set the DAC's gain factor. The value should
-           be 1 or 2.  Gain factor is used to determine output voltage
-           from the formula: Vout = G * Vref * D/4096
-           Where G is gain factor, Vref (for this chip) is 2.048 and
-           D is the 12-bit digital value, defaults to 1
-        :type gain_factor: int, optional
-        :raises ValueError: DAC __init__: Invalid gain factor. Must be 1 or 2
-        """
-        if (gain_factor != 1) and (gain_factor != 2):
-            raise ValueError('DAC __init__: Invalid gain factor. \
-                            Must be 1 or 2')
-        else:
-            self.gain = gain_factor
-            self.maxdacvoltage = self.__dacMaxOutput__[self.gain]
-
-    def read_adc_voltage(self, channel, mode):
-        """
-        [summary]
-        :param channel: 1 or 2
-        :type channel: int
-        :param mode: 0 = single ended, 1 = differential
-        :type mode: int
-        :raises ValueError: read_adc_voltage: channel out of range
-        :raises ValueError: read_adc_voltage: mode out of range
-        :return: voltage
-        :rtype: float
-        """
-        if (channel > 2) or (channel < 1):
-            raise ValueError('read_adc_voltage: channel out of range')
-        if (mode > 1) or (mode < 0):
-            raise ValueError('read_adc_voltage: mode out of range')
-        raw = self.read_adc_raw(channel, mode)
-        voltage = float((self.__adcrefvoltage / 4096) * raw)
-        return voltage
-
-    def read_adc_raw(self, channel, mode):
-        """
-        Read the raw value from the selected channel on the ADC
-        :param channel: 1 or 2
-        :type channel: int
-        :param mode: 0 = single ended, 1 = differential
-        :type mode: int
-        :raises ValueError: read_adc_voltage: channel out of range
-        :raises ValueError: read_adc_voltage: mode out of range
-        :return: raw value from ADC, 0 to 4095
-        :rtype: int
-        """
-        if (channel > 2) or (channel < 1):
-            raise ValueError('read_adc_voltage: channel out of range')
-        if (mode > 1) or (mode < 0):
-            raise ValueError('read_adc_voltage: mode out of range')
-        if mode == 0:
-            raw = self.i2cADC.xfer2([1, (1 + channel) << 6, 0])
-            ret = ((raw[1] & 0x0F) << 8) + (raw[2])
-        if mode == 1:
-            if channel == 1:
-                raw = self.i2cADC.xfer2([1, 0x00, 0])
-            else:
-                raw = self.i2cADC.xfer2([1, 0x40, 0])
-            ret = ((raw[1]) << 8) + (raw[2])
-        return ret
-
-    def set_adc_refvoltage(self, voltage):
-        """
-        Set the reference voltage for the analogue to digital converter.
-        The ADC uses the raspberry pi 3.3V power as a voltage reference so
-        using this method to set the reference to match the
-        exact output voltage from the 3.3V regulator will increase the
-        accuracy of the ADC readings.
-        :param voltage: reference voltage
-        :type voltage: float
-        :raises ValueError: set_adc_refvoltage: reference voltage out of range
-        """
-        if (voltage >= 0.0) and (voltage <= 7.0):
-            self.__adcrefvoltage = voltage
-        else:
-            raise ValueError('set_adc_refvoltage: reference voltage \
-                            out of range')
-        return
-
-    def set_dac_voltage(self, channel, voltage):
-        """
-        Set the voltage for the selected channel on the DAC.
-        The DAC has two gain values, 1 or 2, which can be set when the ADCDAC
-        object is created.
-        A gain of 1 will give a voltage between 0 and 2.047 volts.
-        A gain of 2 will give a voltage between 0 and 3.3 volts.
-        :param channel: 1 or 2
-        :type channel: int
-        :param voltage: DAC target voltage
-        :type voltage: float
-        :raises ValueError: set_dac_voltage: DAC channel needs to be 1 or 2
-        :raises ValueError: set_dac_voltage: voltage out of range
-        """
-        if (channel > 2) or (channel < 1):
-            raise ValueError('set_dac_voltage: DAC channel needs to be 1 or 2')
-        if (voltage >= 0.0) and (voltage < self.maxdacvoltage):
-            rawval = (voltage / 2.048) * 4096 / self.gain
-            self.set_dac_raw(channel, int(rawval))
-        else:
-            raise ValueError('set_dac_voltage: voltage out of range')
-        return
-
-    def set_dac_raw(self, channel, value):
-        """
-        Set the raw value for the selected channel on the DAC
-        :param channel: 1 or 2
-        :type channel: int
-        :param value: 0 and 4095
-        :type value: int
-        :raises ValueError: set_dac_voltage: DAC channel needs to be 1 or 2
-        :raises ValueError: set_dac_voltage: value out of range
-        """
-
-        if (channel > 2) or (channel < 1):
-            raise ValueError('set_dac_voltage: DAC channel needs to be 1 or 2')
-        if (value < 0) and (value > 4095):
-            raise ValueError('set_dac_voltage: value out of range')
-
-        self.dactx[1] = (value & 0xff)
-
-        if self.gain == 1:
-            self.dactx[0] = (((value >> 8) & 0xff) | (channel - 1) << 7 |
-                             1 << 5 | 1 << 4)
-        else:
-            self.dactx[0] = (((value >> 8) & 0xff) | (channel - 1) << 7 |
-                             1 << 4)
-
-        # Write to device
-        self.i2cDAC.xfer2(self.dactx)
-        return
