996d1546e3d2fca003c13e01de560297b813f7a3
maiyama88 <7319136@ed.tus.ac.jp>
2022-11-16 16:20:55 +0900

    大きく変更

--- ADCDACPI.py
+++ ADCDACPI.py
@@ -8,6 +8,7 @@ Based on the Microchip MCP3202 and MCP4822 (new MCP3426)
 
 from __future__ import absolute_import, division, print_function, \
                                                     unicode_literals
+
 try:
     import i2cdev
 except ImportError:
@@ -18,23 +19,55 @@ class ADCDACPi(object):
     """
     Based on the Microchip MCP3202 and MCP4822 (new MCP3426)
     """
+import Adafruit_ADS1x15
+# ADS1015の設定をします
+ads = Adafruit_ADS1x15.ADS1015(address=0x68, busnum=1)
 
-    # variables
-    __adcrefvoltage = 3.3  # reference voltage for the ADC chip.
+# ゲインの設定です
+# 指定によって、取得できる電圧の範囲が変わります
 
-    # Define SPI (new i2c) bus and init
-    i2cADC = i2cdev.I2C_RDWR_S()
-    i2cADC.open(0, 0)
-    i2cADC.max_speed_hz = (1100000)
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+GAIN = 1
 
-    i2cDAC = i2cdev.I2C_RDWR_S()
-    i2cDAC.open(0, 1)
-    i2cDAC.max_speed_hz = (20000000)
+#
+#「ゲイン1」の時の、「受信データ1単位」の電圧を計算します
+#
 
-    dactx = [0, 0]
+# 「ゲイン1」の範囲はプラス・マイナス 4.096なので、8.192になります
+RANGE = 4.096 * 2
 
-    # Max DAC output voltage. Depends on gain factor
-    # The following table is in the form <gain factor>:<max voltage>
+# ADSは上記範囲を12bit(4096)で表すので、上記範囲を4096で割ると
+# ADSのデータ「1」につき、電圧は0.002[V]になります。
+# UNIT=単位
+UNIT = RANGE / 4096
+
+# ループを100回繰り返します
+for i in range(100):
+
+    # ADSのアナログ入力ピンを指定します
+    ads1015_pin  = 0
+
+    data = 0
+    volt = 0
+
+    # ADS1015からデータを取得します
+    data = ads.read_adc( ads1015_pin, gain=GAIN)
+
+    # 取得データから電圧を計算します
+    volt = data * UNIT
+
+    print("受信データ：" + str(data))
+    print("電圧      ：" + "{:.3f}".format(volt))
+
+    # 1秒待機します
+    time.sleep(1)
+
+   
 
     __dacMaxOutput__ = {
         1: 2.048,  # This is Vref
