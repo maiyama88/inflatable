652d89d7f0eecac51fcb7c8ecda47db01d6b00b8
maiyama88 <7319136@ed.tus.ac.jp>
2022-11-16 15:43:58 +0900

    変更

--- ADCDACPI.py
+++ ADCDACPI.py
@@ -23,11 +23,11 @@ class ADCDACPi(object):
     __adcrefvoltage = 3.3  # reference voltage for the ADC chip.
 
     # Define SPI (new i2c) bus and init
-    i2cADC = i2cdev.main()
+    i2cADC = i2cdev.I2C_RDWR_S()
     i2cADC.open(0, 0)
     i2cADC.max_speed_hz = (1100000)
 
-    i2cDAC = i2cdev.main()
+    i2cDAC = i2cdev.I2C_RDWR_S()
     i2cDAC.open(0, 1)
     i2cDAC.max_speed_hz = (20000000)
 
