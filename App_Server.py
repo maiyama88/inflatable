662358c46ab6d0258a2a586434d9e70f368a241c
maiyama88 <7319136@ed.tus.ac.jp>
2022-11-18 18:55:05 +0900

    変更

--- App_Server.py
+++ App_Server.py
@@ -36,8 +36,8 @@ print("IP Address: {0}".format(server.get_ip()))
 
 adc = MCP3426_1()
 
-PRESSURE_LIMIT1 = 3.2   #元は3.13    
-PRESSURE_LIMIT2 = 3.2     
+PRESSURE_LIMIT = 3.2   #元は3.13    
+    
 
 def my_hex(a):
     if a < 0x10 and a >= 0x00:
@@ -68,10 +68,10 @@ if __name__ == '__main__':
             if (time_count % 5000) == 0:
                 # Pressure Check
                 sensor_val1_1 = adc.read_adc(1)
-                sensor_val1 = sensor_val1_1 * 0.7837
+                sensor_val1 = (3.3 * (sensor_val1_1 -9) / (3322 - 9)) 
                 sensor_val2_2 = adc.read_adc(2)
-                sensor_val2 = sensor_val2_2 * 0.7837
-                if sensor_val2 > PRESSURE_LIMIT1 and cmd.Mode == 2:
+                sensor_val2 = (3.3 * (sensor_val2_2 -10) / (3312 - 10))
+                if sensor_val2 > PRESSURE_LIMIT and cmd.Mode == 2:
                     cmd.idle_mode()
                     #if sensor_val2 > PRESSURE_LIMIT2 :
                     #cmd.vent_mode()
