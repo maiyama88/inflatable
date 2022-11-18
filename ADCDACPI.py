3fef7847e1921746297bf9ad00830f2ce29b517c
maiyama88 <7319136@ed.tus.ac.jp>
2022-11-18 18:23:46 +0900

    変更

--- ADCDACPI.py
+++ ADCDACPI.py
@@ -58,23 +58,20 @@ class MCP3426_1():
 	def read_adc(self, channel):
 		"""Read data back from MCP3426_CMD_READ_CNVRSN(0x00), 2 bytes
 		raw_adc MSB, raw_adc LSB"""
-		if channel == 1:
-			CONFIG_CMD_1 = (MCP3426_CMD_MODE_CONT | MCP3426_CMD_SPS_240 | MCP3426_CMD_GAIN_1 | MCP3426_CMD_CHNL_1)
-			bus.write_byte(MCP3426_DEFAULT_ADDRESS, CONFIG_CMD_1)
-		elif channel == 2:
-			CONFIG_CMD_2 = (MCP3426_CMD_MODE_CONT | MCP3426_CMD_SPS_240 | MCP3426_CMD_GAIN_1 | MCP3426_CMD_CHNL_2)
-			bus.write_byte(MCP3426_DEFAULT_ADDRESS, CONFIG_CMD_2)
-
 		data = bus.read_i2c_block_data(MCP3426_DEFAULT_ADDRESS, MCP3426_CMD_READ_CNVRSN, 2)
-
 		if channel == 1:
-			raw_adc = ((data[0] & 0x0F) * 256) + data[1]
-			if raw_adc > 2047 :
-				raw_adc -= 4095
-		if channel == 2:
-			raw_adc = ((data[0] & 0x0F) * 256) + data[1]
-			if raw_adc > 2047 :
-				raw_adc -= 4095
+				CONFIG_CMD_1 = (MCP3426_CMD_MODE_CONT | MCP3426_CMD_SPS_240 | MCP3426_CMD_GAIN_1 | MCP3426_CMD_CHNL_1)
+				bus.write_byte(MCP3426_DEFAULT_ADDRESS, CONFIG_CMD_1)
+				raw_adc = ((data[0] & 0x0F) * 256) + data[1]
+				if raw_adc > 2047 :
+					raw_adc -= 4095
+
+		elif channel == 2:
+				CONFIG_CMD_2 = (MCP3426_CMD_MODE_CONT | MCP3426_CMD_SPS_240 | MCP3426_CMD_GAIN_1 | MCP3426_CMD_CHNL_2)
+				bus.write_byte(MCP3426_DEFAULT_ADDRESS, CONFIG_CMD_2)
+				raw_adc = ((data[0] & 0x0F) * 256) + data[1]
+				if raw_adc > 2047 :
+					raw_adc -= 4095
 		    
 		# Convert the data to 12-bits
 		
