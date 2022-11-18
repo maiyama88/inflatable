0c012dc2101716a08155c0f5d7f11f6aa83f64a7
maiyama88 <7319136@ed.tus.ac.jp>
2022-11-18 18:51:43 +0900

    変更

--- App_Server.py
+++ App_Server.py
@@ -67,10 +67,10 @@ if __name__ == '__main__':
             time_count += 1
             if (time_count % 5000) == 0:
                 # Pressure Check
-                sensor_val1_1 = adc.read_adc.raw_adc(1)
-                sensor_val1 = (3.3 * (sensor_val1_1 -9) / (3322 - 9)) 
-                sensor_val2_2 = adc.read_adc.raw_adc(2)
-                sensor_val2 = (3.3 * (sensor_val2_2 -10) / (3312 - 10))
+                sensor_val1_1 = adc.read_adc(1)
+                sensor_val1 = sensor_val1_1 * 0.7837
+                sensor_val2_2 = adc.read_adc(2)
+                sensor_val2 = sensor_val2_2 * 0.7837
                 if sensor_val2 > PRESSURE_LIMIT1 and cmd.Mode == 2:
                     cmd.idle_mode()
                     #if sensor_val2 > PRESSURE_LIMIT2 :
